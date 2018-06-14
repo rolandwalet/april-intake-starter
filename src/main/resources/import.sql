c4edbd9c43ff224cf6151293c1a5caa15b1c5c34
rolandwalet <roland.walet@gmail.com>
2018-06-14 14:03:48 +0100

    Database and import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Account (id, firstName, secondName, accountNumber) VALUES (1, 'John', 'Doe', '1234');
+INSERT INTO Account (id, firstName, secondName, accountNumber) VALUES (2, 'Jane', 'Doe', '1235');
+INSERT INTO Account (id, firstName, secondName, accountNumber) VALUES (3, 'Jim', 'Taylor', '1236'); 
