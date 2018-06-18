8df39702b6d8763f7fb7beb693dac9e36aa89ae2
rolandwalet <roland.walet@gmail.com>
2018-06-18 14:28:07 +0100

    Updated messaging and changed output of "get" methods to JSON String

--- src/main/java/com/qa/service/AccountServiceDBImpl.java
+++ src/main/java/com/qa/service/AccountServiceDBImpl.java
@@ -21,41 +21,41 @@ public class AccountServiceDBImpl {
 
 	private JSONUtil jsonUtil = new JSONUtil();
 	
-	public List<Account> getAllAccounts() {
+	public String getAllAccounts() {
 		TypedQuery<Account> query = manager.createQuery("SELECT a FROM Account a ORDER BY a.id", Account.class);
-		return query.getResultList();
+		return jsonUtil.getJSONForObject(query.getResultList());
 	}
 
-	public Account getAccountById(long id) {
-		return manager.find(Account.class, id);
+	public String getAccountById(long id) {
+		return jsonUtil.getJSONForObject(manager.find(Account.class, id));
 	}
 
 	@Transactional(REQUIRED)
 	public String createAccount(String account) {
 		Account newAccount = jsonUtil.getObjectForJSON(account, Account.class);
 		manager.persist(newAccount);
-		return "Account successfully created";
+		return "{\"message\": \"Account successfully created\"}";
 	}
 
 	@Transactional(REQUIRED)
 	public String updateAccountById(long id, String account) {
 		Account accountToUpdate = manager.find(Account.class, id);
 		if (accountToUpdate.equals(null)) {
-			return "ERROR: Account does not exist!";
+			return "{\"message\": \"ERROR: Account does not exist!\"}";
 		} else {
 			accountToUpdate.setFirstName(jsonUtil.getObjectForJSON(account, Account.class).getFirstName());
 			accountToUpdate.setSecondName(jsonUtil.getObjectForJSON(account, Account.class).getSecondName());
 			accountToUpdate.setAccountNumber(jsonUtil.getObjectForJSON(account, Account.class).getAccountNumber());
-			return "Account updated!";
+			return "{\"message\": \"Account updated!\"}";
 		}
 	}
 
 	@Transactional(REQUIRED)
 	public String deleteAccountById(long id) {
 		if (manager.find(Account.class, id).equals(null)) {
-			return "ERROR: Account not found!";
+			return "{\"message\": \"ERROR: Account not found!\"}";
 		}
 		manager.remove(manager.find(Account.class, id));
-		return "Account deleted!";
+		return "{\"message\": \"Account deleted!\"}";
 	}
 }
